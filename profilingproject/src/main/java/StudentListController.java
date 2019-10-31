d723eb4e4b45f9f3261057179efe62fda33e7c81
Nail Rogatov <36714810+abnormes@users.noreply.github.com>
2019-10-31 17:30:37 +0300

    [Rogatov] Student add

---
+++ profilingproject/src/main/java/StudentListController.java
@@ -0,0 +1,242 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class StudentListController {
+    @FXML
+    private TableView<Student> studentTableView;
+    @FXML
+    private TableColumn<Student, String> surnameTableColumn;
+    @FXML
+    private TableColumn<Student, String> nameTableColumn;
+    @FXML
+    private TableColumn<Student, String> patronymicTableColumn;
+    @FXML
+    private TableColumn<Student, String> groupTableColumn;
+    @FXML
+    private TableColumn<Student, Double> averageGradeTableColumn;
+    @FXML
+    private TextField searchTextField;
+    @FXML
+    private Label statusLabel;
+    @FXML
+    private Label errorLabel;
+
+    private Main main;
+    ConfirmWindow confirmWindow = new ConfirmWindow();
+
+    @FXML
+    private void initialize() {
+        surnameTableColumn.setCellValueFactory(cellData -> cellData.getValue().surnameProperty());
+        nameTableColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        patronymicTableColumn.setCellValueFactory(cellData -> cellData.getValue().patronymicProperty());
+        groupTableColumn.setCellValueFactory(cellData -> cellData.getValue().groupProperty());
+        averageGradeTableColumn.setCellValueFactory(cellData -> cellData.getValue().averageGradeProperty().asObject());
+
+        statusLabel.setText("Elements in table: " + studentTableView.getItems().size());
+
+        studentTableView.getSelectionModel().getSelectedCells().addListener(new ListChangeListener<TablePosition>() {
+            @Override
+            public void onChanged(Change<? extends TablePosition> c) {
+                errorLabel.setText("");
+            }
+        });
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+        studentTableView.setItems(main.getStudentList());
+    }
+
+    @FXML
+    private void handleNewFile() {
+        if (!main.getStudentList().isEmpty()) {
+            boolean answer = confirmWindow.showConfirmWindow("Confirm create a new file",
+                    "Are you sure you want to create a new list without saving?");
+            if (answer) {
+                main.getStudentList().removeAll(main.getStudentList());
+                errorLabel.setText("");
+                statusLabel.setText("Elements in table: " + studentTableView.getItems().size());
+            }
+        }
+    }
+
+    @FXML
+    private void handleOpenFile() {
+        boolean answer = true;
+        if (!main.getStudentList().isEmpty()) {
+            answer = confirmWindow.showConfirmWindow("Confirm open file",
+                    "Are you sure you want to open file without saving?");
+        }
+        if (answer) {
+            main.getStudentList().removeAll(main.getStudentList());
+
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XLSX files", "*.xlsx"));
+
+            List<File> files = fileChooser.showOpenMultipleDialog(null);
+            if (files != null) {
+                errorLabel.setText("");
+                try {
+                    readFromExcel(files);
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void readFromExcel(List<File> files) throws IOException{
+        String fullname = "";
+        String group = "";
+        double averageGrade = 0;
+
+        for(File file : files) {
+            XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(file));
+            XSSFSheet sheet = myExcelBook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            Row row = rowIterator.next();
+
+            while(rowIterator.hasNext()) {
+
+                row = rowIterator.next();
+
+                if (row.getCell(1).getCellType() == XSSFCell.CELL_TYPE_STRING && row.getCell(1) != null && !row.getCell(1).equals("")) {
+                    fullname = row.getCell(1).getStringCellValue();
+                } else {
+                    continue;
+                }
+
+
+                if (row.getCell(2).getCellType() == XSSFCell.CELL_TYPE_STRING && row.getCell(2) != null && !row.getCell(2).equals("")) {
+                    group = row.getCell(2).getStringCellValue();
+                } else {
+                    group = "Неизвестно";
+                }
+
+                if (row.getCell(3).getCellType() == XSSFCell.CELL_TYPE_NUMERIC && row.getCell(3) != null && !row.getCell(3).equals("")) {
+                    averageGrade = row.getCell(3).getNumericCellValue();
+                } else {
+                    averageGrade = 0;
+                }
+
+                String name[] = fullname.split(" ");
+                main.getStudentList().add(new Student(name[0], name[1], name[2], group, averageGrade));
+            }
+
+            myExcelBook.close();
+        }
+    }
+
+    @FXML
+    private void handleMergeWithFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XLSX files", "*.xlsx"));
+
+        List<File> files = fileChooser.showOpenMultipleDialog(null);
+        if (files != null) {
+            errorLabel.setText("");
+            try {
+                readFromExcel(files);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void handleDeleteStudent() {
+        int selectedItem = studentTableView.getSelectionModel().getSelectedIndex();
+        if (selectedItem >= 0) {
+            boolean answer = confirmWindow.showConfirmWindow("Confirm remove selected person",
+                    "Are you sure you want to remove selected item?");
+            if (answer) {
+                studentTableView.getItems().remove(selectedItem);
+                statusLabel.setText("Elements in table: " + studentTableView.getItems().size());
+            }
+        } else {
+            errorLabel.setText("No student selected!");
+        }
+    }
+
+    @FXML
+    private void handleAddStudent() {
+        Student tempStudent = new Student();
+        boolean okClicked = main.showStudentEditDialog(tempStudent);
+        if (okClicked) {
+            main.getStudentList().add(tempStudent);
+            statusLabel.setText("Elements in table: " + studentTableView.getItems().size());
+        }
+    }
+
+    @FXML
+    private void handleEditStudent() {
+        Student selectedStudent = studentTableView.getSelectionModel().getSelectedItem();
+        if (selectedStudent != null) {
+            main.showStudentEditDialog(selectedStudent);
+        } else {
+            errorLabel.setText("No student selected!");
+        }
+    }
+
+    @FXML
+    private void handleAboutProgram() {
+        showAboutWindow();
+    }
+
+    private void showAboutWindow() {
+        Stage stage = new Stage();
+        stage.setTitle("About Program");
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
+        Label lbl1 = new Label("Developed by");
+        Label lbl2 = new Label("group IKPI-61");
+        Label lbl3 = new Label("in 2019");
+        Button btnOk = new Button("OK");
+        btnOk.setOnAction(event -> stage.close());
+        vbox.getChildren().addAll(lbl1, lbl2, lbl3, btnOk);
+        Scene scene = new Scene(vbox, 250, 140);
+        stage.setScene(scene);
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(main.getPrimaryStage());
+        stage.showAndWait();
+    }
+
+    @FXML
+    private void handleExitProgram() {
+        showConfirmExitWindow();
+    }
+
+    public void showConfirmExitWindow() {
+        boolean answer = confirmWindow.showConfirmWindow("Confirm exit",
+                "Are you sure you want to exit the program without saving?");
+        if (answer) {
+            System.exit(1);
+        }
+    }
+}
